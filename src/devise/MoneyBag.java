ea81141d165f21b2e421a1f942251e7fe0ab0910
lolalfg <56671149+lolalfg@users.noreply.github.com>
2020-12-04 17:12:24 +0100

    commit

--- src/devise/MoneyBag.java
+++ src/devise/MoneyBag.java
@@ -1,37 +1,80 @@
 package devise;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MoneyBag {
 	
-	private ArrayList<Money> moneyBagList;
-	
-	public MoneyBag(ArrayList<Money> moneyBagList) {
-		this.moneyBagList = moneyBagList;
-	}
-	
-	public MoneyBag Normalise(String currency) {
-		for(Money monnaie : this.moneyBagList) {
-			monnaie.setfCurrency(currency);
-		}
-		
-		return this;
-	}
-	
-	public MoneyBag Add(Money monnaie) {
-		if(monnaie.getfCurrency().equals(this.moneyBagList.get(0).getfCurrency()))
-			this.moneyBagList.add(monnaie);
-		
-		return this;
-	}
+	public HashMap<String, Money> monnaies;
 
+    public MoneyBag() {
+        this.monnaies = new HashMap<String, Money>();
+    }
 
-	public ArrayList<Money> getMoneyBagList() {
-		return moneyBagList;
-	}
+    public HashMap<String, Money> moneys() {
+        return this.monnaies;
+    }
 
-	public void setMoneyBagList(ArrayList<Money> moneyBagList) {
-		this.moneyBagList = moneyBagList;
-	}
+    public MoneyBag add(Money monnaie){
+        if (this.monnaies.containsKey(monnaie.currency())) {
+            this.monnaies.get(monnaie.currency()).add(monnaie);
+        } else {
+            this.monnaies.put(monnaie.currency(), new Money(monnaie.amount(), monnaie.currency()));
+        }
+        return this;
+    }
+
+    public MoneyBag add(MoneyBag moneyBag){
+        for (Map.Entry<String, Money> entry : moneyBag.monnaies.entrySet()) {
+            this.add(entry.getValue());
+        }
+        return this;
+    }
+
+    public MoneyBag subb(Money monnaie) throws Exception {
+        if (this.monnaies.containsKey(monnaie.currency())){
+            this.monnaies.get(monnaie.currency()).add(-monnaie.amount(), monnaie.currency());
+        } else {
+            throw new Exception();
+        }
+
+        return this;
+    }
+
+    public MoneyBag subb(MoneyBag moneyBag) throws Exception {
+        for (Map.Entry<String, Money> entry : moneyBag.monnaies.entrySet()) {
+            this.subb(entry.getValue());
+        }
+        return this;
+    }
+
+    public Double normalise() {
+        Double total = 0.00;
+        for (Map.Entry<String, Money> entry : this.monnaies.entrySet()) {
+            switch (entry.getKey()) {
+                case "EUR":
+                    total += entry.getValue().amount();
+                    break;
+                case "USD":
+                    total += (entry.getValue().amount() * 0.82);
+                    break;
+                case "CHF":
+                    total += (entry.getValue().amount() * 0.92);
+                    break;
+                case "GBP":
+                    total += (entry.getValue().amount() * 1.10);
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+        return total;
+    }
+
+    public Boolean normaliseEqual(MoneyBag moneyBag) {
+        return (this.normalise().equals(moneyBag.normalise()));
+    }
 
 }
--- test/devises/MoneyBagTest.java
+++ test/devises/MoneyBagTest.java
@@ -3,45 +3,187 @@ package devises;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import devise.Money;
 import devise.MoneyBag;
 
+
 public class MoneyBagTest {
 
-private MoneyBag moneyBag;
-	
-	public MoneyBagTest() {
-		Money monnaie1 = new Money(20, "EUR");
-		Money monnaie2 = new Money(40, "EUR");
-		Money monnaie3 = new Money(60, "EUR");
-		
-		
-		ArrayList<Money> moneyBagList = new ArrayList<Money>();
-		moneyBagList.add(monnaie1);
-		moneyBagList.add(monnaie2);
-		moneyBagList.add(monnaie3);
-		
-		this.moneyBag = new MoneyBag(moneyBagList);
-	}
-	
-	@Test
-	public void NormaliseTest() {
-		this.moneyBag.Normalise("GBP").getMoneyBagList().forEach(m -> assertEquals(m.currency(), "GBP"));
-	}
-	
-	@Test
-	public void AjoutTest() {
-		Money ajoutMoney = new Money(50, "GBP");
-		int size = this.moneyBag.getMoneyBagList().size();
-		
-		this.moneyBag.Add(ajoutMoney);
-		
-		assertEquals(size++, this.moneyBag.getMoneyBagList().size());
-	}
+	MoneyBag globalBag;
+
+    @BeforeEach
+    public void moneyBagCreation() {
+        Money monnaie1, monnaie2, monnaie3, monnaie4;
+        this.globalBag = new MoneyBag();
+
+        try {
+        	monnaie1 = new Money(10, "EUR");
+        	monnaie2 = new Money(10, "USD");
+        	monnaie3 = new Money(10, "CHF");
+        	monnaie4 = new Money(10, "GBP");
+
+            this.globalBag.add(monnaie1);
+            this.globalBag.add(monnaie2);
+            this.globalBag.add(monnaie3);
+            this.globalBag.add(monnaie4);
+
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    MoneyBag moneyBagCreationSpe() {
+        Money monnaie1, monnaie2, monnaie3, monnaie4;
+        MoneyBag moneyBag = new MoneyBag();
+
+        try {
+        	monnaie1 = new Money(10, "EUR");
+        	monnaie2 = new Money(10, "USD");
+        	monnaie3 = new Money(10, "CHF");
+        	monnaie4 = new Money(10, "GBP");
+
+            moneyBag.add(monnaie1);
+            moneyBag.add(monnaie2);
+            moneyBag.add(monnaie3);
+            moneyBag.add(monnaie4);
+
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+        return moneyBag;
+    }
+
+    @Test
+    public void moneyBagCreationTaille() {
+        MoneyBag moneybag = new MoneyBag();
+        assertEquals(0, moneybag.monnaies.size());
+    }
+
+    @Test
+    public void ajoutMoney() {
+        Money monnaie;
+        try {
+        	monnaie = new Money(50, "EUR");
+            this.globalBag.add(monnaie);
+
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+        assertEquals(20, this.globalBag.monnaies.get("EUR").amount());
+    }
+
+    @Test
+    public void ajoutMoneyBag() {
+        MoneyBag ajout = moneyBagCreationSpe();
+        try {
+            this.globalBag.add(ajout);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+        for (Map.Entry<String, Money> entry : this.globalBag.monnaies.entrySet()) {
+            assertEquals(20, entry.getValue().amount());
+        }
+        assertEquals(4, this.globalBag.monnaies.size());
+    }
+
+    @Test
+    public void ajoutMoneyNonExistante() {
+        MoneyBag moneyBag = new MoneyBag();
+        try {
+            moneyBag.add(new Money(150, "EUR"));
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+        assertEquals(150, moneyBag.monnaies.get("EUR").amount());
+        assertEquals(1, moneyBag.monnaies.size());
+    }
+
+    @Test
+    public void ajoutMoneyBagNonExistante() {
+        MoneyBag moneyBag = new MoneyBag();
+        try {
+            moneyBag.add(this.globalBag);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+        for (Map.Entry<String, Money> entry : moneyBag.monnaies.entrySet()) {
+            assertEquals(50, entry.getValue().amount());
+        }
+        assertEquals(4, moneyBag.monnaies.size());
+    }
+
+    @Test
+    public void subMoneyExistante() {
+        Money monnaie;
+        try {
+        	monnaie = new Money(25, "EUR");
+            this.globalBag.subb(monnaie);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+        for (Map.Entry<String, Money> entry : this.globalBag.monnaies.entrySet()) {
+            if (entry.getKey() == "EUR") {
+                assertEquals(25, entry.getValue().amount());
+            } else {
+                assertEquals(50, entry.getValue().amount());
+            }
+
+        }
+        assertEquals(4, this.globalBag.monnaies.size());
+
+    }
+
+    @Test
+    public void subMoneyNonExistante() {
+        Money monnaie;
+        MoneyBag moneyBag = new MoneyBag();
+        try {
+        	monnaie = new Money(35, "EUR");
+
+            Exception thrown = assertThrows(
+                Exception.class,
+                () -> moneyBag.subb(monnaie),
+                "Pas bon"
+            );
+
+            String messageException = "Devise incorrect";
+            String message = thrown.getMessage();
+            assertSame(message, messageException);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void subMoneyBag() {
+        Money monnaie;
+        try {
+        	monnaie = new Money(25, "EUR");
+            this.globalBag.subb(monnaie);
+
+            assertEquals(25, this.globalBag.monnaies.get("EUR").amount());
+            assertEquals(4, this.globalBag.monnaies.size());
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+
 
 }
